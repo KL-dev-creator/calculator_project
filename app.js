e66ca0ce46eab8c7eda4b37d201b8b9e42075c7b
KL-dev-creator <155812039+KL-dev-creator@users.noreply.github.com>
2024-10-14 00:12:14 +1300

    Tested functionality for number keys

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let firstNumber = 0;
 let operator = "";
 let secondNumber = 0;
 
+let currentNumber = "";
+
 
 function add(a, b) {
 
@@ -59,44 +61,66 @@ const oneButton = document.querySelector('.one')
 const twoButton = document.querySelector('.two')
 
 one.addEventListener('click', () => {
-    console.log("One has been pressed")
+    console.log("One has been pressed");
+    currentNumber += "1";
+    console.log(currentNumber);
     })
 
 two.addEventListener('click', () => {
     console.log("Two has been pressed")
+    currentNumber += "2";
+    console.log(currentNumber);
     })
 
 three.addEventListener('click', () => {
     console.log("Three has been pressed")
+    currentNumber += "3";
+    console.log(currentNumber);
     })
     
 four.addEventListener('click', () => {
     console.log("Four has been pressed")
+    currentNumber += "4";
+    console.log(currentNumber);
     })
 
 five.addEventListener('click', () => {
     console.log("Five has been pressed")
+    currentNumber += "5";
+    console.log(currentNumber);
     })
 
 six.addEventListener('click', () => {
     console.log("Six has been pressed")
+    currentNumber += "6";
+    console.log(currentNumber);
     })
 
 seven.addEventListener('click', () => {
     console.log("Seven has been pressed")
+    currentNumber += "7";
+    console.log(currentNumber);
     })
 
 eight.addEventListener('click', () => {
     console.log("Eight has been pressed")
+    currentNumber += "8";
+    console.log(currentNumber);
     })
 
 nine.addEventListener('click', () => {
     console.log("Nine has been pressed")
+    currentNumber += "9";
+    console.log(currentNumber);
     })
 
 zero.addEventListener('click', () => {
     console.log("Zero has been pressed")
+    currentNumber += "0";
+    console.log(currentNumber);
     })
+
+// const calculatorArray = [1,2,3,4,5,6,7,8,9,0,+,-,*,/];
 
 clear.addEventListener('click', () => {
     const displayedText = document.querySelector(".display");

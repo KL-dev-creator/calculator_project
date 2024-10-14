4a15366e9ac6f5c6f53a1adf8fe6c02c32bd01f3
KL-dev-creator <155812039+KL-dev-creator@users.noreply.github.com>
2024-10-15 00:40:14 +1300

    Able to make numbers display along with operators and clear button

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let firstNumber = 0;
 let operator = "";
 let secondNumber = 0;
 
+const displayedText = document.querySelector(".display");
+
 let currentNumber = "";
 
 
@@ -57,72 +59,112 @@ console.log(operate(12, "*", 4))
 console.log(operate(12, "/", 4))
 console.log(operate(12, "S", 4))
 
-const oneButton = document.querySelector('.one')
-const twoButton = document.querySelector('.two')
-
 one.addEventListener('click', () => {
     console.log("One has been pressed");
-    currentNumber += "1";
+    displayedText.textContent += "1";
+    currentNumber += 1;
     console.log(currentNumber);
     })
 
 two.addEventListener('click', () => {
     console.log("Two has been pressed")
-    currentNumber += "2";
+    displayedText.textContent += "2";
+    currentNumber += 2;
     console.log(currentNumber);
     })
 
 three.addEventListener('click', () => {
     console.log("Three has been pressed")
-    currentNumber += "3";
+    displayedText.textContent += "3";
+    currentNumber += 3;
     console.log(currentNumber);
     })
     
 four.addEventListener('click', () => {
     console.log("Four has been pressed")
-    currentNumber += "4";
+    displayedText.textContent += "4";
+    currentNumber += 4;
     console.log(currentNumber);
     })
 
 five.addEventListener('click', () => {
     console.log("Five has been pressed")
-    currentNumber += "5";
+    displayedText.textContent += "5";
+    currentNumber += 5;
     console.log(currentNumber);
     })
 
 six.addEventListener('click', () => {
     console.log("Six has been pressed")
-    currentNumber += "6";
+    displayedText.textContent += "6";
+    currentNumber += 6;
     console.log(currentNumber);
     })
 
 seven.addEventListener('click', () => {
     console.log("Seven has been pressed")
-    currentNumber += "7";
+    displayedText.textContent += "7";
+    currentNumber += 7;
     console.log(currentNumber);
     })
 
 eight.addEventListener('click', () => {
     console.log("Eight has been pressed")
-    currentNumber += "8";
+    displayedText.textContent += "8";
+    currentNumber += 8;
     console.log(currentNumber);
     })
 
 nine.addEventListener('click', () => {
     console.log("Nine has been pressed")
-    currentNumber += "9";
+    displayedText.textContent += "9";
+    currentNumber += 9;
     console.log(currentNumber);
     })
 
 zero.addEventListener('click', () => {
     console.log("Zero has been pressed")
-    currentNumber += "0";
+    displayedText.textContent += "0";
+    currentNumber += 0;
     console.log(currentNumber);
     })
 
+addition.addEventListener('click', () => {
+    console.log("+ has been pressed")
+    displayedText.textContent = "+";
+    console.log(currentNumber);
+    })
+
+subtraction.addEventListener('click', () => {
+    console.log("- has been pressed")
+    displayedText.textContent = "-";
+    console.log(currentNumber);
+    })
+
+multiplication.addEventListener('click', () => {
+    console.log("x has been pressed")
+    displayedText.textContent = "x";
+    console.log(currentNumber);
+    })
+
+division.addEventListener('click', () => {
+    console.log("/ has been pressed")
+    displayedText.textContent = "/";
+    console.log(currentNumber);
+    })
+
+equals.addEventListener('click', () => {
+    console.log("= has been pressed")
+    displayedText.textContent += "=";
+    console.log(currentNumber);
+    })
+
+
 // const calculatorArray = [1,2,3,4,5,6,7,8,9,0,+,-,*,/];
 
+
+
 clear.addEventListener('click', () => {
-    const displayedText = document.querySelector(".display");
-    displayedText.textContent = "12345"
+
+    displayedText.textContent = ""
     })
